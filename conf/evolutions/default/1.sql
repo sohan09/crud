04e82473beb21ce15e103a29ccce6afa2bbfc283
sohan <sohan09014@gmail.com>
2014-08-23 04:29:14 +0600

    b4

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,56 +4,72 @@
 # --- !Ups
 
 create table complex (
-  id                        bigint auto_increment not null,
+  id                        bigint not null,
   string_field              varchar(255),
   long_string_field         varchar(256),
   integer_field             integer,
   double_field              double,
-  boolean_field             tinyint(1) default 0,
-  date_field                datetime,
+  boolean_field             boolean,
+  date_field                timestamp,
   constraint pk_complex primary key (id))
 ;
 
 create table product (
-  id                        bigint auto_increment not null,
+  id                        bigint not null,
   name                      varchar(256),
   description               varchar(756),
   user_id                   bigint,
-  created_date              datetime,
-  last_modified_date        datetime,
+  created_date              timestamp,
+  last_modified_date        timestamp,
   constraint pk_product primary key (id))
 ;
 
 create table simple (
-  id                        bigint auto_increment not null,
+  id                        bigint not null,
   name                      varchar(255),
   constraint pk_simple primary key (id))
 ;
 
 create table user (
-  id                        bigint auto_increment not null,
+  id                        bigint not null,
   first_name                varchar(50),
   last_name                 varchar(50),
   email                     varchar(100),
-  created_date              datetime,
-  last_modified_date        datetime,
+  created_date              timestamp,
+  last_modified_date        timestamp,
   constraint pk_user primary key (id))
 ;
+
+create sequence complex_seq;
+
+create sequence product_seq;
+
+create sequence simple_seq;
+
+create sequence user_seq;
 
 
 
 
 # --- !Downs
 
-SET FOREIGN_KEY_CHECKS=0;
+SET REFERENTIAL_INTEGRITY FALSE;
 
-drop table complex;
+drop table if exists complex;
 
-drop table product;
+drop table if exists product;
 
-drop table simple;
+drop table if exists simple;
 
-drop table user;
+drop table if exists user;
 
-SET FOREIGN_KEY_CHECKS=1;
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists complex_seq;
+
+drop sequence if exists product_seq;
+
+drop sequence if exists simple_seq;
+
+drop sequence if exists user_seq;
 
