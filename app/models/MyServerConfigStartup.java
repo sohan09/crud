88c55b6730c6d424a4c2831caa56304a65ca7fa9
sohan <sohan09014@gmail.com>
2014-08-23 05:08:37 +0600

    vv

--- app/models/MyServerConfigStartup.java
+++ app/models/MyServerConfigStartup.java
@@ -1,11 +1,39 @@
 package models;
 
 import com.avaje.ebean.config.ServerConfig;
+import com.avaje.ebean.config.DataSourceConfig;
 import com.avaje.ebean.event.ServerConfigStartup;
+
+import java.net.*;
 
 public class MyServerConfigStartup implements ServerConfigStartup {
     @Override
     public void onStart(ServerConfig serverConfig) {
+	
         serverConfig.setDatabaseSequenceBatchSize(1);
+		
+		URI dbUri;
+		
+		try {
+			
+			dbUri = new URI(System.getenv("DATABASE_URL"));
+			
+		} catch (URISyntaxException ex) {
+		
+			return;
+		}
+		
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+		
+		DataSourceConfig dataSourceConfig = new DataSourceConfig();
+		
+		dataSourceConfig.setUrl(dbUrl);
+		dataSourceConfig.setPassword(password);
+		dataSourceConfig.setUsername(username);
+		
+		serverConfig.setDataSourceConfig(dataSourceConfig);
     }
+	
 }
