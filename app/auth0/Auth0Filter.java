652cd58e809b9bf54c95a4132f68e2ef67972cd6
sohan <sohan09014@gmail.com>
2014-08-24 10:08:56 +0600

    663

---
+++ app/auth0/Auth0Filter.java
@@ -0,0 +1,52 @@
+package auth0;
+
+import java.io.IOException;
+
+import play.mvc.*;
+import play.data.*;
+import play.api.Play;
+import static play.mvc.Controller.*;
+
+public class Auth0Filter {
+
+    private String onFailRedirectTo;
+
+    @Override
+    public void init() {
+        onFailRedirectTo = Play.current.configuration.getString("auth0.redirect_on_error");
+
+        if (onFailRedirectTo == null) {
+            throw new IllegalArgumentException("auth0.redirect_on_authentication_error parameter of " + this.getClass().getName() + " cannot be null");
+        }
+    }
+
+    protected Tokens loadTokens() {
+
+        return new Tokens((String) session("idToken"),
+                (String) session("accessToken"));
+    }
+
+    protected void onSuccess(Tokens tokens) throws IOException {
+		
+    }
+
+    protected void onReject() throws IOException {
+		
+    }
+
+    public void doFilter() throws IOException {
+
+        Tokens tokens = loadTokens();
+
+        // Reject if not accessToken or idToken are found
+        if (tokens.getAccessToken() == null || tokens.getIdToken() == null) {
+            onReject();
+            return;
+        }
+
+        onSuccess(tokens);
+    }
+
+    public void destroy() {
+    }
+}
---
+++ app/auth0/Auth0RequestWrapper.java
@@ -0,0 +1,20 @@
+package auth0;
+
+import play.mvc.*;
+import play.data.*;
+import static play.mvc.Controller.*;
+
+public class Auth0RequestWrapper {
+    String idToken;
+
+    public Auth0RequestWrapper(Tokens tokens) {
+
+        this.idToken = tokens.getIdToken();
+    }
+
+    public Principal getUserPrincipal() {
+	
+        return new Auth0Principal(this.idToken);
+    }
+
+}
