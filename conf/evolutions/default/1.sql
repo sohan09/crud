2747102f8adfb4b53489bbd92c5fda9147e81c74
dayspringltd <reza@dayspringlimited.com>
2014-08-25 13:24:14 +0600

    afa

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,6 +3,17 @@
 
 # --- !Ups
 
+create table complex (
+  id                        bigint not null,
+  string_field              varchar(255),
+  long_string_field         varchar(256),
+  integer_field             integer,
+  double_field              float,
+  boolean_field             boolean,
+  date_field                timestamp,
+  constraint pk_complex primary key (id))
+;
+
 create table product (
   id                        bigint not null,
   name                      varchar(256),
@@ -11,6 +22,12 @@ create table product (
   created_date              timestamp,
   last_modified_date        timestamp,
   constraint pk_product primary key (id))
+;
+
+create table simple (
+  id                        bigint not null,
+  name                      varchar(255),
+  constraint pk_simple primary key (id))
 ;
 
 create table p_user (
@@ -23,7 +40,11 @@ create table p_user (
   constraint pk_p_user primary key (id))
 ;
 
+create sequence complex_seq;
+
 create sequence product_seq;
+
+create sequence simple_seq;
 
 create sequence p_user_seq;
 
@@ -32,11 +53,19 @@ create sequence p_user_seq;
 
 # --- !Downs
 
+drop table if exists complex cascade;
+
 drop table if exists product cascade;
+
+drop table if exists simple cascade;
 
 drop table if exists p_user cascade;
 
+drop sequence if exists complex_seq;
+
 drop sequence if exists product_seq;
+
+drop sequence if exists simple_seq;
 
 drop sequence if exists p_user_seq;
 
