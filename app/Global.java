fe6a6ce156b5441bc9b43a8666cad3acf5a4d4b8
dayspringltd <reza@dayspringlimited.com>
2014-08-25 13:17:30 +0600

    3sr

--- app/Global.java
+++ app/Global.java
@@ -76,10 +76,5 @@ public class Global extends GlobalSettings {
     public void onStop(Application app) {
         Logger.info("Application shutdown...");
     } */
-	
-    public Action onRequest(Request request, Method actionMethod) {
-        System.out.println("before each request..." + request.toString());
-        return super.onRequest(request, actionMethod);
-    }
 
 }
